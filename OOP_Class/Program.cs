0d8b13cc1b6424c9457ccba754f43ec3583cd857
selinozluk <selinozluk36@gmail.com>
2025-07-08 13:20:46 +0300

    Update code

--- OOP_Class/Program.cs
+++ OOP_Class/Program.cs
@@ -1,15 +1,20 @@
 ﻿using OOP_Class;
 using System;
 
-Person person1 = new Person();
-person1.Name = "Billie";
-person1.Surname = "Eilish";
-person1.Birthdate = new DateTime(2001, 12, 18);
 
+// 1. kişi nesnesi
+Person person1 = new Person();
+person1.Name = "Billie";  // ad bilgisi
+person1.Surname = "Eilish"; // soyad bilgisi
+person1.Birthdate = new DateTime(2001, 12, 18); // doğum tarihi
+
+// 1. kişinin bilgileri yazdırılacak
 Console.WriteLine($"Kişinin adı: {person1.Name}");
 Console.WriteLine($"Kişinin soyadı: {person1.Surname}");
 Console.WriteLine($"Kişinin doğum tarihi: {person1.Birthdate.ToString("dd.MM.yyyy")}");
 
+
+// 2. kişi nesnesi
 Person person2 = new Person
 {
     Name = "Finneas",
@@ -17,6 +22,7 @@ Person person2 = new Person
     Birthdate = new DateTime(1997, 7, 30)
 };
 
+// 2. kişinin bilgileri yazdırılacak
 Console.WriteLine("---------- 2. Kişi ----------");
 Console.WriteLine($"Ad: {person2.Name}");
 Console.WriteLine($"Soyad: {person2.Surname}");
