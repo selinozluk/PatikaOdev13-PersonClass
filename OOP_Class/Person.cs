dfa2c874df1b435ece0ed71be9a2968b75bbf19b
selinozluk <selinozluk36@gmail.com>
2025-07-08 12:59:58 +0300

    Add project files.

---
+++ OOP_Class/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Class
+{
+    // Nesne oluşturuyoruz
+    internal class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Birthdate { get; set; } 
+    }
+}
---
+++ OOP_Class/Program.cs
@@ -0,0 +1,36 @@
+﻿using OOP_Class;
+using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
+using System.Xml.Linq;
+
+string name1 = "Billie";
+string surname1 = "Eilish";
+int birthdate1 = 2001;
+
+
+Person person1 = new Person(); // Nesne oluşturuldu
+
+person1.Name = "Billie";
+person1.Surname = "Eilish";
+person1.Birthdate = 2001;
+
+
+Console.WriteLine($"Kişinin adı: {person1.Name}");
+Console.WriteLine($"Kişinin soyadı: {person1.Surname}");
+Console.WriteLine($"Kişinin doğum tarihi: {person1.Birthdate}");
+
+// 2. Yol
+
+Person person2 = new Person
+{
+
+Name = "Finneas",
+Surname = "O'Connel",
+Birthdate = 1997,
+
+};
+
+Console.WriteLine("---------- 2. Kişi ----------");
+Console.WriteLine($"Ad: {person2.Name}");
+Console.WriteLine($"Soyad: {person2.Surname}");
+Console.WriteLine($"Doğum Tarihi: {person2.Birthdate}");
